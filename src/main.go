91e1573fa9d57b1ab06eba93262b2d433793b80e
Vimal Hirapara <Vimal.Hirapara@contis.com>
2022-12-30 17:33:04 +0530

    first commit

---
+++ src/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+)
+
+type Server interface {
+	Address() string
+	IsAlive() bool
+	Serve(w http.ResponseWriter, r *http.Request)
+}
+
+type simpleServer struct {
+	addr  string
+	proxy *httputil.ReverseProxy
+}
+
+func newSimpleServer(addr string) *simpleServer {
+	serverUrl, err := url.Parse(addr)
+
+	handleErr(err)
+
+	return &simpleServer{addr: addr, proxy: httputil.NewSingleHostReverseProxy(serverUrl)}
+}
+
+type LoadBalancer struct {
+	port            string
+	roundRobinCount int
+	servers         []Server
+}
+
+func NewLoadBalancer(port string, servers []Server) *LoadBalancer {
+	return &LoadBalancer{
+		port:            port,
+		roundRobinCount: 0,
+		servers:         servers,
+	}
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+	os.Exit(1)
+}
+
+func (s *simpleServer) Address() string {
+	return s.addr
+}
+
+func (s *simpleServer) IsAlive() bool {
+	return true
+}
+
+func (s *simpleServer) Serve(w http.ResponseWriter, r *http.Request) {
+	s.proxy.ServeHTTP(w, r)
+}
+
+func (lb *LoadBalancer) getNextAvailableServer() Server {
+	server := lb.servers[lb.roundRobinCount%len(lb.servers)]
+
+	for !server.IsAlive() {
+		lb.roundRobinCount++
+		server = lb.servers[lb.roundRobinCount%len(lb.servers)]
+	}
+	lb.roundRobinCount++
+	return server
+}
+
+func (lb *LoadBalancer) serverProxy(w http.ResponseWriter, r *http.Request) {
+	targetServer := lb.getNextAvailableServer()
+	fmt.Printf("forwarding request to address %q\n", targetServer.Address())
+	targetServer.Serve(w, r)
+}
+
+func main() {
+	Servers := []Server{
+		newSimpleServer("https://facebook.com"),
+		newSimpleServer("https://bing.com"),
+		newSimpleServer("https://duckduckgo.com"),
+	}
+
+	lb := NewLoadBalancer(":8000", Servers)
+
+	handleRedirect := func(w http.ResponseWriter, r *http.Request) {
+		lb.serverProxy(w, r)
+	}
+	http.HandleFunc("/", handleRedirect)
+
+	fmt.Printf("serving request at 'localhost:%s'\n", lb.port)
+	http.ListenAndServe(":"+lb.port, nil)
+}
